fe0dc3c66fa88ef9642937b9b515c8b11d83771c
Sam9476 <155827937+Sam9476@users.noreply.github.com>
2025-02-28 21:36:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from joblib import load
+from numpy import array
+
+import streamlit as st
+
+# load the model
+model = load("model.pkl")
+
+st.title("Placement Package Prediction")
+st.write("Enter CGPA")
+
+cgpa_input = st.number_input("CGPA",
+                             max_value=10.0,
+                             min_value=0.0,
+                             step=0.1)
+
+
+if st.button("Predict"): 
+    inputf = array([[cgpa_input]])
+    prediction = model.predict(inputf)[0]
+    st.success(f"Predicted Package: {prediction:.2f}")
